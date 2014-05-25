34aa2f9ee04b17356051d19ee0700ce41f1697d3
Joe Burgess <joe@flatironschool.com>
2014-05-25 09:07:06 -0400

    adding in updated pods

---
+++ locationTrivia-Objects/Pods/Pods-locationTrivia-ObjectsTests-OCHamcrest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_locationTrivia_ObjectsTests_OCHamcrest : NSObject
+@end
+@implementation PodsDummy_Pods_locationTrivia_ObjectsTests_OCHamcrest
+@end
