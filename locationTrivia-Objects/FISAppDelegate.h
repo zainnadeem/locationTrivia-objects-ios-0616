53859b5dbd857c3f61af128f49041af86c29382e
markedwardmurray <markedwardmurray@gmail.com>
2015-08-31 18:37:04 -0400

    remove solution

--- locationTrivia-Objects/FISAppDelegate.h
+++ locationTrivia-Objects/FISAppDelegate.h
@@ -8,14 +8,4 @@
 
 @property (strong, nonatomic) UIWindow *window;
 
-@property (strong, nonatomic) NSMutableArray *locations;
-
-- (NSArray *)allLocationNames;
-
-- (FISLocation *)locationNamed:(NSString *)name;
-
-- (NSArray *)locationsNearLatitude:(CGFloat)latitude
-                         longitude:(CGFloat)longitude
-                            margin:(CGFloat)margin;
-
 @end
